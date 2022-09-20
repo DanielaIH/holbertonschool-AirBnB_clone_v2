a71efe2745b2c8690f4c5a2bacfab942a2eda919
DanielaIH <4427@holbertonschool.com>
2022-09-19 22:10:12 -0500

    UPDATE app.url_map.strict_slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,10 @@ from flask import Flask
 
 
 app = Flask(__name__)
-@app.route("/", strict_slashes=False)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
 def hello_hbnb():
     """hello HBNB"""
     return "Hello HBNB!"
